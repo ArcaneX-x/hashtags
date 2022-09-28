960ae2c6bf4807ead29eb3899a59c44bc0e1eb95
axelrod <fasolshik@yahoo.com>
2022-09-28 10:46:37 +0600

    add main.rb

--- lib/hashtags.rb
+++ lib/hashtags.rb
@@ -2,4 +2,3 @@ def hashtags(string)
   string.scan(/#[[:word:]:?-]+/)
 end
 
-puts hashtags('Всем #Привет! #kak_dela? #woohoo #ha-ha!')
---
+++ main.rb
@@ -0,0 +1,3 @@
+require_relative 'lib/hashtags'
+
+puts hashtags('Всем #Привет! #kak_dela? #woohoo #ha-ha!')
